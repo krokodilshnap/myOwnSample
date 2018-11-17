3e07bb277c5324afbe7e42c88cdfcef77c5df81d
Dmitriy <krokodilshnap@gmail.com>
2018-11-17 23:35:42 +0300

    Add config file smartgrid.js

--- smartgrid.js
+++ smartgrid.js
@@ -2,7 +2,36 @@ const smartgrid = require('smart-grid');
 
 const settings = {
 	filename: "_smartGrid",
-	outputStyle: 'scss'
+	outputStyle: 'scss',
+	columns: 12,
+	offset: '30px',
+	container: {
+		maxWidth: "1565px",
+		fields: "10px"
+	},
+	breakPoints: {
+		xlg: {
+			width: "1400px",
+			fields: "15px"
+		},
+		lg: {
+			width: "1279px"
+		},
+		md: {
+			width: "991px"
+
+		},
+		sm: {
+			width: "767px"
+		},
+		ssm: {
+			width: "600px",
+			fields: "10px"
+		},
+		xs: {
+			width: "479px"
+		}
+	}
 };
 
 smartgrid('./app/scss/components', settings);
